d3a9ff450b40fdf914b63a4a043a3877a0ae0d8c
Artem0904 <moonportal708@gmail.com>
2023-06-14 14:14:44 +0300

    Details panel (view)

---
+++ 04_BookLibrary_HOME/ViewModels/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+namespace _04_BookLibrary_HOME.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    internal class Book
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateOnly ReleaseDate { get; set; }
+
+        public Book(string Title, string Description, string Autor, DateOnly ReleaseDate)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.Author = Autor;
+            this.ReleaseDate = ReleaseDate;
+        }
+        public override string ToString()
+        {
+            return $"{Title} by {Author}    {ReleaseDate.Year}"; 
+        }
+
+    }
+}
---
+++ 04_BookLibrary_HOME/ViewModels/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_BookLibrary_HOME.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    internal class ViewModel
+    {
+        private ObservableCollection<Book> library = new ObservableCollection<Book>();
+
+        public IEnumerable<Book> Library => library;
+
+        public Book SelectedBook { get; set; }
+
+
+
+    }
+}
